20346972180119adcc76bf2a049b381bfa93d800
Catalin-Ioan-Sapariuc <sapariuc@yahoo.com>
2025-03-08 08:31:36 +0200

    added a new page (long term todos in pages) and a new theme through configuration file

--- pages/Long-term-todos.py
+++ pages/Long-term-todos.py
@@ -1,23 +1,33 @@
 import streamlit as st 
-import functions
+import sys
 
-todos = functions.get_todos()
+def get_todos(filepath='long_term_todos.txt'):
+    with open(filepath,'r') as file:
+            todos_local = file.readlines()
+    return todos_local
+
+def write_todos(todos_list, filepath='long_term_todos.txt'):
+    with open(filepath,'w') as file:
+            file.writelines(todos_list)
+    return 0
+
+todos = get_todos()
 
 def add_todo():
     todo = st.session_state["new_todo"]+"\n"
     todos.append(todo)
-    functions.write_todos(todos)
+    write_todos(todos)
 
 #st.subheader("This should be a cool to do app!")
 
-st.write("Welcome to your to do app!")
+st.write("This page is for you to add your long terms todos!")
 
 
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key =todo)
     if checkbox:
         todos.pop(index)
-        functions.write_todos(todos)
+        write_todos(todos)
         del st.session_state[todo]
         st.rerun()
 
